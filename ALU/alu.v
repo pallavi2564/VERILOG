cce1bf311199208841d5688c2bb7bc9a73998ddd
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-07-23 20:08:55 +0530

    Update alu.v

--- ALU/alu.v
+++ ALU/alu.v
@@ -1,18 +1,18 @@
 module alu(input[7:0] A,
            input[7:0] B,
            input[3:0] sel,
-           output[7:0] out);
+           output reg[7:0] out);
   reg [7:0]O;
 always@(*)begin
   case(sel)
-  0:O<=16'h0000;
+  0:O<=8'h0000;
   1:O<=A&B;
   2:O<=A|B;
   3:O<=A^B;
   4:O<=~A;
   5:O<=A-B;
   6:O<=A+B;
-  7:O<=16'hFFFF;
+  7:O<=8'hFFFF;
   endcase
   end
   assign out=O;
