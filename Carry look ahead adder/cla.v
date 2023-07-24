6c87d0c4ff28a312cbc28959476fa4447d3bbd73
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-07-24 15:20:35 +0530

    Create cla.v

---
+++ Carry look ahead adder/cla.v
@@ -0,0 +1,19 @@
+module cla(
+  input [3:0]A, B, 
+  input Cin,
+  output reg [3:0] S,
+  output reg Cout
+);
+  wire [3:0] Ci; 
+  
+  assign Ci[0] = Cin;
+  assign Ci[1] = (A[0] & B[0]) | ((A[0]^B[0]) & Ci[0]);
+ 
+  assign Ci[2] = (A[1] & B[1]) | ((A[1]^B[1]) & ((A[0] & B[0]) | ((A[0]^B[0]) & Ci[0])));
+  
+  assign Ci[3] = (A[2] & B[2]) | ((A[2]^B[2]) & ((A[1] & B[1]) | ((A[1]^B[1]) & ((A[0] & B[0]) | ((A[0]^B[0]) & Ci[0])))));
+ 
+  assign Cout  = (A[3] & B[3]) | ((A[3]^B[3]) & ((A[2] & B[2]) | ((A[2]^B[2]) & ((A[1] & B[1]) | ((A[1]^B[1]) & ((A[0] & B[0]) | ((A[0]^B[0]) & Ci[0])))))));
+
+  assign S = A^B^Ci;
+endmodule
