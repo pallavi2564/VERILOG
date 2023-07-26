49ce81b923c9c35346155637f36db96297946862
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-07-26 21:04:55 +0530

    Create priority_encoder_tb.v

---
+++ 4 to 2 Priority Encoder/priority_encoder_tb.v
@@ -0,0 +1,16 @@
+module priority_encoder_tb;
+  reg a0,a1;
+  wire y0,y1,y2,y3;
+  
+  priority_encoder uut(a0,a1,y0,y1,y2,y3);
+  
+  initial begin
+   a0=1,a1=0;
+    #10;
+    a0=0,a1=1;
+    #10;
+    a0=1,a1=1;
+    #10;
+    a0=0;a1=0;
+  end
+endmodule
