343abb23c19ebdd9cd50ccc0068d766c4523c23b
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-07-27 15:51:35 +0530

    Create comparator_tb.v

---
+++ 4 Bit Comparator/comparator_tb.v
@@ -0,0 +1,27 @@
+module comparator_tb;
+
+   
+  reg [3:0] a;
+  reg [3:0]b;
+    wire l;
+    wire e;
+    wire g;
+
+  comparator uut (
+       a,b,l,e,g
+    );
+
+    initial begin
+        
+        a = 10;
+        b= 12;
+        #100;
+        a = 15;
+       b = 11;
+        #100;
+       a = 10;
+        b = 10;
+        #100;
+    end
+      
+endmodule
