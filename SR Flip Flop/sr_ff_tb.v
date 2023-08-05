f0bc752bbaa86aaa5c481a8459af8f0c2b5846af
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-08-05 22:09:32 +0530

    Update sr_ff_tb.v

--- SR Flip Flop/sr_ff_tb.v
+++ SR Flip Flop/sr_ff_tb.v
@@ -1,7 +1,7 @@
 module sr_ff_tb;
   wire clk,rst,s,r;
   reg q;
-  sr_ff uut(clk,rst,s,r,q);
+  sr_ff uut(.clk(clk),.rst(rst),.s(s),.r(r),.q(q));
   $monitor("simtime=%g,clk=%b,rst=%b,s=%b,r=%b,q=%b,%time,clk,rst,s,r,q);
      initial begin
        clk=0;
