ab48764564f03bd3154fd26a3408aadcba01eee1
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-08-05 22:07:41 +0530

    Update fs_tb.v

--- Full Subtractor/fs_tb.v
+++ Full Subtractor/fs_tb.v
@@ -6,7 +6,7 @@ module fs_tb;
   
 initial begin
   $monitor("At time %0t: a=%b b=%b, Bin=%b, D=%b, Bout=%b",$time, a,b,Bin,D,Bout);
-    a = 0; b = 0; Bin = 0; 
+  a = 0; b = 0; Bin = 0; 
   #10 a = 0; b = 0; Bin = 1;
   #10a = 0; b = 1; Bin = 0;
   #10 a = 1; b = 0; Bin = 0; 
