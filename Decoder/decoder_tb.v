fe836dd879f0a5f9928390941a86318befb677c1
pallavi2564 <127688670+pallavi2564@users.noreply.github.com>
2023-08-05 22:05:02 +0530

    Update decoder_tb.v

--- Decoder/decoder_tb.v
+++ Decoder/decoder_tb.v
@@ -2,7 +2,7 @@ module decoder_tb;
   reg a0,a1;
   wire y0,y1,y2,y3;
   
-  priority_encoder uut(a0,a1,y0,y1,y2,y3);
+  priority_encoder uut(.a0(a0),.a1(a1),.y0(y0),.y1(y1),.y2(y2),.y3(y3));
   
   initial begin
    a0=1,a1=0;
