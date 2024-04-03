820379d5c6aae0932a5e9ff25ff5bda2b20fc206
Pallavi <yampalakupallavi@gmail.com>
2024-04-03 22:47:36 +0530

    Create ring_tb.v

---
+++ Ring Counter/ring_tb.v
@@ -0,0 +1,21 @@
+module ring_tb();
+reg clk, reset;
+wire [3:0] q;
+ring dut( .clk(clk), .reset(reset), .q(q));
+
+initial
+begin
+clk = 0;
+reset = 1;
+#10;
+reset = 0;
+end
+
+always #5 clk = ~clk;
+
+initial
+begin
+$monitor (" reset = %b, q = %b", reset, q);
+end
+
+endmodule
